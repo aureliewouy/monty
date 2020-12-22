da5aa320bfa47da2637ad5c9c68a368ef8ae3c11
aureliewouy <aureliecedia@yahoo.com>
2020-12-22 14:38:33 +0000

    add error handling

--- main.c
+++ main.c
@@ -16,13 +16,14 @@ int main(int argc, char **argv)
 
 	delim = " \n";
 	if (argc != 2)
-	{       /*Error: ;*/
+	{
+		dprintf(STDOUT_FILENO, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
-		/*Error: Can't open file <file>*/
+		dprintf(STDOUT_FILENO, "Error: Can't open file %s", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (getline(&buffer, &bufsize, file) != -1)

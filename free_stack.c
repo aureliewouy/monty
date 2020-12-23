bb7a7b5c73bfa25d8f5bf3a3ced1fa95ded97b1a
Natalia Ponsard <2503@holbertonschool.com>
2020-12-23 09:39:01 +0000

    noir

--- free_stack.c
+++ free_stack.c
@@ -3,24 +3,11 @@
 /**
  * free_stack - frees the stack on exit
  * @status: exit status
+ * @arg: a pointer to the stack
  *
  */
 void free_stack(int status, void *arg)
 {
 	(void)status;
 	(void)arg;
-/*listint_t *ptr, *hold;
-
-	if (head == NULL)
-		return;
-
-	ptr = head;
-
-	while (ptr != NULL)
-	{
-		hold = ptr->next;
-		free(ptr);
-		ptr = hold;
-	}*/
-
 }

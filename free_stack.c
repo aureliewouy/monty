c33d22be332d665590ace3ee4dbcc04ff4e6bb96
Natalia Ponsard <2503@holbertonschool.com>
2020-12-23 11:45:45 +0000

    changes

--- free_stack.c
+++ free_stack.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-
 /**
  * free_stack - frees the stack on exit
  * @status: exit status
@@ -8,6 +7,6 @@
  */
 void free_stack(int status, void *arg)
 {
-	(void)status;
-	(void)arg;
+(void)status;
+(void)arg;
 }
--- main.c
+++ main.c
@@ -39,6 +39,5 @@ int main(int argc, char **argv)
 
 	}
 	exit(EXIT_SUCCESS);
-	fclose(file);
 	return (0);
 }

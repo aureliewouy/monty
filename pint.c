7baafcd8e52155ba3223c6e21b1967cc8ee7708c
aureliewouy <aureliecedia@yahoo.com>
2020-12-23 13:56:23 +0000

    add stdout

--- pint.c
+++ pint.c
@@ -12,7 +12,7 @@ void function_pint(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		dprintf(STDERR_FILENO, "L%d: can't pint, stack empty\n", line_number);
+		dprintf(STDOUT_FILENO, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);

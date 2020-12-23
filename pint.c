e1c4ef233c57e1f43e8ef09482ddb74e5e597cbe
aureliewouy <aureliecedia@yahoo.com>
2020-12-23 13:59:12 +0000

    add pint

--- pint.c
+++ pint.c
@@ -10,11 +10,13 @@
 
 void function_pint(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL)
+	stack_t *head = *stack;
+
+	if (head == NULL)
 	{
-		dprintf(STDOUT_FILENO, "L%d: can't pint, stack empty\n", line_number);
+		dprintf(STDERR_FILENO, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", (*stack)->n);
+	printf("%d\n", head->n);
 
 }
